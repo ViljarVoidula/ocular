df54969e647a099b34f4e967f7446c934b268e2d
Viljar Võidula <viljar@testreel.com>
2021-11-22 22:33:21 +0200

    health check

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,22 @@
-import compress from 'compression';
-import helmet from 'helmet';
-import cors from 'cors';
+import compress from "compression";
+import helmet from "helmet";
+import cors from "cors";
 
+import express from "@feathersjs/express";
+import feathers from "@feathersjs/feathers";
+import configuration from "@feathersjs/configuration";
+import { Application } from "./declarations";
 
-import express from '@feathersjs/express';
-import feathers from '@feathersjs/feathers';
-import configuration from '@feathersjs/configuration';
-import { Application } from './declarations';
+import caching from "./cache";
 
-import caching from './cache';
+import nats from "./nats";
+import mongodb from "./userdb";
+import localdb from "./localdb";
 
-import nats from './nats'
-import mongodb from './userdb';
-import localdb from './localdb';
-
-import logger from './logger';
-import services from './services';
-import appHooks from './app.hooks';
-import authentication from './authentication';
-
+import logger from "./logger";
+import services from "./services";
+import appHooks from "./app.hooks";
+import authentication from "./authentication";
 
 const app: Application = express(feathers());
 
@@ -29,20 +27,25 @@ app.use(cors());
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/', express.static(, {}));
-app.use("/", express.static(app.get('public')))
-// app.use('/', express.static(app.get('public')));
-app.configure(caching)
-app.configure(nats)
-app.configure(mongodb)
-app.configure(localdb)
+app.use("/", express.static(app.get("public")));
+app.configure(caching);
+app.configure(nats);
+app.configure(mongodb);
+app.configure(localdb);
 
 app.configure(express.rest());
 
-app.configure(authentication)
+app.configure(authentication);
 app.configure(services);
+app.get("/health", (req, res) => {
+  const data = {
+    uptime: process.uptime(),
+    message: "OK",
+    date: new Date(),
+  };
 
-
+  res.status(200).send(data);
+});
 // Configure a middleware for 404s and the error handler
 app.use(express.notFound());
 app.use(express.errorHandler({ logger }));
