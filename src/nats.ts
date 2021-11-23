ef2badcc09102ff5a153c5ee808f08c6e2aa1305
Viljar Võidula <viljar@testreel.com>
2021-11-23 09:31:54 +0200

    nats config change

--- src/nats.ts
+++ src/nats.ts
@@ -1,46 +1,49 @@
-import { connect, StringCodec } from 'nats'
-import { Application } from './declarations';
+import { connect, StringCodec } from "nats";
+import { Application } from "./declarations";
 
-
-function optionsToString(options: { [key: string]: any }){
-    let result = ''
-    Object.keys(options).forEach((key, index)=>{
-        if(options[key] ){
-            result += key + '=' + options[key]
-        }
-    })
-    return result?.replace(/\/|\./gi, '-');
+function optionsToString(options: { [key: string]: any }) {
+  let result = "";
+  Object.keys(options).forEach((key, index) => {
+    if (options[key]) {
+      result += key + "=" + options[key];
+    }
+  });
+  return result?.replace(/\/|\./gi, "-");
 }
 
-export default function(app: Application){
-    const {servers = []} = app.get('nats')
+export default function (app: Application) {
+  const { servers = [], user, pass } = app.get("nats");
 
-    if(servers.length){
-        (async()=>{
-            const connection = await connect({servers});
-            console.log(`max payload for the server is ${connection?.info?.max_payload} bytes`)
-      
-            // connection.closed();
-            const sub = connection.subscribe("resize_image");
-            (async () => {
-                for await (const m of sub) {
-                  app.emit('resize_image_in', StringCodec().decode(m.data))
-                }
-              })();
-            console.info('Connected to NATS')
-       
-            
-            app.set('natsClient', connection)
-            
-        })() 
-    }
+  if (servers.length) {
+    (async () => {
+      const connection = await connect({ servers, user, pass });
+      console.log(
+        `max payload for the server is ${connection?.info?.max_payload} bytes`
+      );
 
-    app.on('resize_image_in',(data: string)=>{
-        const _data = JSON.parse(data)
-        const _instance =  app.services.resize.instanceId
-        const cache = app.get('_cache')
-        if(_instance !== _data.instance){
-            cache.set(optionsToString(_data.query), _data.map(el=>Buffer.from(el.data)), { ttl: 600 })
+      // connection.closed();
+      const sub = connection.subscribe("resize_image");
+      (async () => {
+        for await (const m of sub) {
+          app.emit("resize_image_in", StringCodec().decode(m.data));
         }
-    })
+      })();
+      console.info("Connected to NATS");
+
+      app.set("natsClient", connection);
+    })();
+  }
+
+  app.on("resize_image_in", (data: string) => {
+    const _data = JSON.parse(data);
+    const _instance = app.services.resize.instanceId;
+    const cache = app.get("_cache");
+    if (_instance !== _data.instance) {
+      cache.set(
+        optionsToString(_data.query),
+        _data.map((el) => Buffer.from(el.data)),
+        { ttl: 600 }
+      );
+    }
+  });
 }
